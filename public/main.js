b39b3b653b193668457c491e6b98259683e95970
Philip Krause <philipkrause7@gmail.com>
2019-05-07 09:12:07 -0400

    f3

--- public/main.js
+++ public/main.js
@@ -4,61 +4,49 @@ let maxSec = 60
 let maxMin = 1
 
 const myTimer = () => {
-  if (maxMin == 0) {
-    return 0
-  } else {
-    if (maxSec > 0 && maxMin > 0) {
-      maxSec -= 1
-    } else {
-      maxSec = 60
-      maxMin -= 1
-    }
-    document.querySelector('.timerSecond').textContent = maxSec
-    document.querySelector('.timerMinute').textContent = maxMin
+  if (maxSec > 0 && maxMin >= 0) {
+    maxSec -= 1
   }
+  if (maxSec === 0 && maxMin > 0) {
+    maxSec = 60
+    maxMin -= 1
+  }
+  if (maxSec === 0 && maxMin === 0) {
+    maxSec = 0
+    maxMin = 0
+  }
+  document.querySelector('.timerSecond').textContent = maxSec
+  document.querySelector('.timerMinute').textContent = maxMin
 }
 const oneSecondCount = setInterval(myTimer, 1000)
 
-// if ((maxSec = 0)) {
-//   maxSec += 0
-//   maxMin -= 1
-//   document.querySelector('.timerMinute').textContent = maxMin
-// }
-
 const updateTeam1 = () => {
   const team1 = document.querySelector('.team-name1').value
-  console.log(team1)
   document.querySelector('.teamName1').textContent = team1
 }
 
 const updateTeam2 = () => {
-  console.log('team 2 button was pushed')
   const team2 = document.querySelector('.team-name2').value
   document.querySelector('.teamName2').textContent = team2
 }
 
 const buttonTeamScore1 = () => {
-  console.log('button was clicked')
   teams1Score += 1
   document.querySelector('.team1Score').textContent = teams1Score
 }
 const buttonTeamScore2 = () => {
-  console.log('button3 was clicked')
   teams2Score += 1
   document.querySelector('.team2Score').textContent = teams2Score
 }
 
 const buttonTeamScoreSub1 = () => {
-  console.log('button2 was clicked')
   teams1Score -= 1
   document.querySelector('.team1Score').textContent = teams1Score
 }
 const buttonTeam2ScoreSub = () => {
-  console.log('button4was clicked')
   teams2Score -= 1
   document.querySelector('.team2Score').textContent = teams2Score
 }
-
 document
   .querySelector('.team-1-add-1-button')
   .addEventListener('click', buttonTeamScore1)
